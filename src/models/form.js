8252eb0edb3896a54aec218c62e829b471ed3a54
Blitz1r <david.ranigo@gmail.com>
2024-10-19 17:09:07 +0800

    created the form mongoose model

---
+++ src/models/form.js
@@ -0,0 +1,101 @@
+const mongoose = require('mongoose');
+
+
+const formSchema = new mongoose.Schema({
+    //Pick-up Informatiom
+    pickupProvince: { 
+        type: String,
+        required: true
+    },
+    pickupCity: { 
+        type: String,
+        required: true
+    },
+    pickupBarangay: { 
+        type: String,
+        required: true
+    },
+    pickupStreet: { 
+        type: String,
+        required: true
+    },
+    pickupBuilding: { 
+        type: String,
+        required: true
+    },
+    pickupDate: { 
+        type: Date,
+        required: true
+    },
+    pickupTime: { 
+        type: String,
+        required: true
+    },
+    pickupPassengers: {
+        type: Number,
+        min: 1,
+        required: true
+    },
+
+    //Departure Information
+    departureProvince: { 
+        type: String,
+        required: true
+    },
+    departureCity: { 
+        type: String,
+        required: true
+    },
+    departureBarangay: { 
+        type: String,
+        required: true
+    },
+    departureStreet: { 
+        type: String,
+        required: true
+    },
+    departureBuilding: { 
+        type: String,
+        required: true
+    },
+    departureDate: { 
+        type: Date,
+        required: true
+    },
+    departureTime: { 
+        type: String,
+        required: true
+    },
+    departureReturnDate: { 
+        type: Date
+    },
+    departureAddInformation: { 
+        type: String
+    },
+
+    //Contact Information
+    contactCompanyName: { 
+        type: String
+    },
+    contactPerson: {
+        type: String,
+        required: true
+    },
+    contactEmail: {
+        type: String,
+        required: true
+    },
+    contactNumber: {
+        type: String,
+        required: true
+    },
+    
+    deleted: {
+        type: Boolean,
+        default: 0
+    }
+});
+
+const Form = mongoose.model('Form', formSchema);
+
+module.exports = Form;
